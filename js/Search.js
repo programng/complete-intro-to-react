5be4cb271783ab73407344c43908a9677619c854
Jonathan Ng <jon7ng@gmail.com>
2017-02-23 20:33:37 -0800

    take out showcard, add keys

--- js/Search.js
+++ js/Search.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
@@ -7,14 +8,7 @@ const Search = React.createClass({
       <div className='search'>
         {preload.shows.map(function(show) {
           return (
-            <div className='show-card'>
-              <img src={`/public/img/posters/${show.poster}`} />
-              <div>
-                <h3>{show.title}</h3>
-                <h4>({show.year})</h4>
-                <p>{show.description}</p>
-              </div>
-            </div>
+            <ShowCard key={show.imdbID} show={show} />
           )
         })}
       </div>
